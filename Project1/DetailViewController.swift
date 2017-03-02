c4f730ada7454f917dba46cb4fa49080d09aa31b
chibitofu <erinmoony@gmail.com>
2017-03-01 21:00:45 -0800

    Cleaned up some code.

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -21,23 +21,9 @@ class DetailViewController: UIViewController {
             imageView.image  = UIImage(named: imageToLoad)
         }
         
-        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
